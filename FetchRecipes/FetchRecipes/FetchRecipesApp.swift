18d04e74e825e3849d37deff5c01214cc986d08e
Oren Leavitt <oleavitt@ix.netcom.com>
2024-10-07 21:58:30 -0500

    Initial project skeleton

---
+++ FetchRecipes/FetchRecipes/FetchRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipesApp.swift
+//  FetchRecipes
+//
+//  Created by Oren Leavitt on 10/7/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
